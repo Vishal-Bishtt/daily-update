2f2c6e0838d82c89bb93fe7a97b5b0d7aa7db577
Vishal-Bishtt <vishalbisht2005@gmail.com>
2024-12-31 13:00:51 +0530

    successfull

---
+++ fileHandling/writing.java
@@ -0,0 +1,19 @@
+package fileHandling.writing;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class writing {
+    public static void main(String[] args) {
+        String name = "NewFile.txt";
+        try{
+            FileWriter writer = new FileWriter(name);
+            writer.write("This is my first file in java.");
+            writer.flush();
+            System.out.println("The file has been flushed");
+        }catch(IOException e){
+            System.out.println("An error has occured!!");
+        }
+
+    }
+}
