6a94706067885479b3396276a6f9fd886bb03220
Vishal-Bishtt <vishalbisht2005@gmail.com>
2025-01-04 02:07:44 +0530

    Success

---
+++ arrayList/lst.java
@@ -0,0 +1,9 @@
+package arrayList;
+//
+//import java.util.List;
+import java.util.*;
+public class lst {
+    public static void main(String[] args) {
+        List lst = new ArrayList();
+    }
+}
---
+++ wrapperClasses.java
@@ -0,0 +1,6 @@
+public class wrapperClasses {
+    public static void main(String[] args) {
+        Integer first = 55; //is immutable
+        Integer second = Integer.valueOf(55); // is same as above
+    }
+}
